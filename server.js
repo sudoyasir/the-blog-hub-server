34246fd0b898fc00580331ae04c5385813c6ff3c
yasir2002 <y451rmahar@gmail.com>
2023-04-21 18:21:40 +0500

    initial setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const port = 3030;
 
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+mongoose.connect('mongodb+srv://yasir2002:EliteBook2002@cluster0.vgcb7fd.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log("MongoDB Atlas is connected to server!");
+    app.listen(port, () => {
+        console.log(`server is running on port ${port}`);
+    })
+}).catch((error) => {
+    console.log("db NOT connected");
+    console.log(error.message);
+})
